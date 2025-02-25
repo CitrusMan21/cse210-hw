b766bf97c241fe1d398ab056ffcc15a54e677326
Darrin <citrusman20@gmail.com>
2025-02-25 09:53:38 -0700

    added code:

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello CSE 210");
+        Console.WriteLine("Please Enter Your Name: ");
+        string Name = Console.ReadLine();
+        Console.WriteLine($"{Name}");
     }
 }
