a7f8bf3435359a0e55aaa1f7dc971c4808681842
Darrin <citrusman20@gmail.com>
2025-02-26 09:34:48 -0700

    James Bond Intro Remake

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Please Enter Your First Name?: ");
+        Console.WriteLine("Please Enter Your Second Name?: ");
+        string FirstName = Console.ReadLine();
+        string LastName = Console.ReadLine();
+        Console.WriteLine($"Your Name Is {LastName}, {FirstName} {LastName}");
     }
 }
