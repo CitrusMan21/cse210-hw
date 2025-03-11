d41c31123f65b24ef134ca2e2f341e13d2aa06a2
Darrin <citrusman20@gmail.com>
2025-03-11 09:51:01 -0600

    Fraction Entry

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction(3, 4);
+        Console.WriteLine("Fraction 1: " + fraction1.GetFractionString());
+        Console.WriteLine("Decimal Value: " + fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine("\nFraction 2: " + fraction2.GetFractionString());
+        Console.WriteLine("Decimal Value: " + fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(2, 5);
+        Console.WriteLine("\nFraction 3: " + fraction3.GetFractionString());
+        Console.WriteLine("Decimal Value: " + fraction3.GetDecimalValue());
     }
 }
